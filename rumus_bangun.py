060eedefba170174deaa20856eb67a3a331c801c
07_Berliana Cahya Wijaya <liana.tkjsmeksa25@gmail.com>
2026-09-04 08:13:23 +0700

    rumus_bangun.py

---
+++ rumus_bangun.py
@@ -0,0 +1,11 @@
+def hitung_persegi_luas(sisi_bangun):
+    return sisi_bangun * sisi_bangun
+
+def hitung_persegi_keliling(sisi_bangun):
+    return 4 * sisi_bangun
+
+def hitung_lingkaran_luas(r_lingkaran):
+    return 3.14 * r_lingkaran * r_lingkaran
+
+def hitung_segitiga_luas(a_segitiga, t_segitiga):
+    return 0.5 * a_segitiga * t_segitiga
